0fbb52a1184f6481d574952bd4f975902895575b
Gustavo Feliciano Figueiredo <gustavo.flcn@gmail.com>
2020-07-23 09:57:28 -0300

    Aula 1

---
+++ UberTutorial/UberTutorial/Controller/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  UberTutorial
+//
+//  Created by Gustavo Feliciano Figueiredo on 23/07/20.
+//  Copyright © 2020 Gustavo Feliciano Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
