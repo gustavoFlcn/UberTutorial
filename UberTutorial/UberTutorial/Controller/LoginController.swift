d78cce6d07af7fdd8cdbd9e329efacdf19efef14
Gustavo Feliciano Figueiredo <gustavo.flcn@gmail.com>
2020-07-23 22:02:33 -0300

    Aula 2, Aula 3, Aula 4, Aula 5 e Aula 6

--- UberTutorial/UberTutorial/Controller/LoginController.swift
+++ UberTutorial/UberTutorial/Controller/LoginController.swift
@@ -10,8 +10,88 @@ import UIKit
 
 class LoginController: UIViewController{
     
+    //MARK: Properties
+    
+    // TITLE
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    // EMAIL
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        return UITextField().textfield(withPlaceholder: "Email", isSecureTextEntry: false)
+    }()
+    
+    // PASSWORD
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+    }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textfield(withPlaceholder: "Password", isSecureTextEntry: true)
+    }()
+    
+    //Login Button
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(UIColor(white: 1, alpha: 0.5 ), for: .normal)
+        button.backgroundColor = UIColor.mainBlueTint
+        button.layer.cornerRadius = 5
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    //dont have account button
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributeTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes:
+            [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+             NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        attributeTitle.append(NSAttributedString(string: "Sign In", attributes:
+            [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16),
+             NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint ]))
+        button.setAttributedTitle(attributeTitle, for: .normal)
+        return button
+    }()
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor.backgroundColor
+        
+        //Adicionando Titulo
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 24
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16 )
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)
+    
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
     }
 }
