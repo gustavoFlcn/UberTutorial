ff03f282dcf33c191ead467203b2dab15a7dc99e
Gustavo Feliciano Figueiredo <gustavo.flcn@gmail.com>
2020-07-23 22:42:02 -0300

    Aula 7

--- UberTutorial/UberTutorial/Controller/LoginController.swift
+++ UberTutorial/UberTutorial/Controller/LoginController.swift
@@ -61,9 +61,12 @@ class LoginController: UIViewController{
         let attributeTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes:
             [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
              NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        attributeTitle.append(NSAttributedString(string: "Sign In", attributes:
+        attributeTitle.append(NSAttributedString(string: "Sign Un", attributes:
             [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16),
              NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint ]))
+        
+        button.addTarget(self, action: #selector(handleShowSingUp), for: .touchUpInside)
+        
         button.setAttributedTitle(attributeTitle, for: .normal)
         return button
     }()
@@ -71,8 +74,22 @@ class LoginController: UIViewController{
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.backgroundColor
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    
+    @objc func handleShowSingUp(){
+        let controller = SignUpController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    //MARK: - Helper functions
+    func configureUI(){
+        configureNavigationBar()
         
+        view.backgroundColor = UIColor.backgroundColor
+            
         //Adicionando Titulo
         view.addSubview(titleLabel)
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
@@ -83,15 +100,15 @@ class LoginController: UIViewController{
         stack.distribution = .fillEqually
         stack.spacing = 24
         view.addSubview(stack)
-        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16 )
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16)
         
         view.addSubview(dontHaveAccountButton)
         dontHaveAccountButton.centerX(inView: view)
         dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)
-    
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle{
-        return .lightContent
+    func configureNavigationBar(){
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
     }
 }
---
+++ UberTutorial/UberTutorial/Controller/SignUpController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpController.swift
+//  UberTutorial
+//
+//  Created by Gustavo Feliciano Figueiredo on 23/07/20.
+//  Copyright © 2020 Gustavo Feliciano Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController{
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    //MARK: - Selectors
+    
+}
